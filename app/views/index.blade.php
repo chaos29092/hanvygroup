3f27cd342f4a823931c20cf3809e29f410903032
chaos29092 <chaos29092@gmail.com>
2014-11-05 23:22:07 +0800

    index done

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -14,7 +14,7 @@
 
         <link rel="stylesheet" href="assets/css/normalize.css">
         <link rel="stylesheet" href="assets/css/main.css">
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css">
         <script src="assets/js/vendor/modernizr-2.6.2.min.js"></script>
     </head>
     <body>
@@ -22,11 +22,162 @@
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-        <!-- Add your site or application content here -->
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <div id="top">
+            <div class="container">
+                <a href="/"><img id="logo" src="{{asset('img/logo.jpg')}}" alt=""/></a>
+                <p class="pull-right"><a href="">English</a>  |  <a href="">Pусский</a>  |  <a href="">中文</a></p>
+            </div>
+        </div>
+
+        <nav class="nav navbar-default" role="navigation" id="nav">
+          <div class="container">
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+                <a href="#"><button type="button" class="btn btn-sky text-uppercase btn-lg">home</button></a>
+                <a href="#"><button type="button" class="btn btn-sky text-uppercase btn-lg">products</button></a>
+                <a href="#"><button type="button" class="btn btn-sky text-uppercase btn-lg">factory tours</button></a>
+                <a href="#"><button type="button" class="btn btn-sky text-uppercase btn-lg">solution</button></a>
+                <a href="#"><button type="button" class="btn btn-sky text-uppercase btn-lg">service</button></a>
+                <a href="#"><button type="button" class="btn btn-sky text-uppercase btn-lg">customer visit</button></a>
+                <a href="#"><button type="button" class="btn btn-sky text-uppercase btn-lg">about us</button></a>
+                <a href="#"><button type="button" class="btn btn-sky text-uppercase btn-lg">why us</button></a>
+                <a href="#"><button type="button" class="btn btn-sky text-uppercase btn-lg">contact</button></a>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+
+        {{--Carousel--}}
+        <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+          </ol>
+
+          <!-- Wrapper for slides -->
+          <div class="carousel-inner" role="listbox" id="carousel">
+            <div class="item active">
+              <img src="{{asset('img/banner1.jpg')}}" class="img-responsive" alt="...">
+            </div>
+            <div class="item">
+              <img src="{{asset('img/banner2.jpg')}}" class="img-responsive" alt="...">
+            </div>
+          </div>
+        </div>
+
+        {{--product list--}}
+        <div class="container" id="productList">
+            <h4>PRODUCTS</h4>
+        </div>
+        <div class="container" id="productImg">
+            <div class="row">
+              <div class="col-xs-6 col-md-3">
+                <a href="#" class="thumbnail">
+                  <img src="{{asset('img/index/1.jpg')}}" alt="...">
+                  <div class="caption">
+                    <h4 class="text-center">hot asphalt mixing plant
+                                            LB3000 (240TPH) Asphalt Mixing Plant</h4>
+                  </div>
+                </a>
+              </div>
+              <div class="col-xs-6 col-md-3">
+                <a href="#" class="thumbnail">
+                  <img src="{{asset('img/index/1.jpg')}}" alt="...">
+                  <div class="caption">
+                    <h4 class="text-center">hot asphalt mixing plant
+                                            LB3000 (240TPH) Asphalt Mixing Plant</h4>
+                  </div>
+                </a>
+              </div>
+              <div class="col-xs-6 col-md-3">
+                <a href="#" class="thumbnail">
+                  <img src="{{asset('img/index/1.jpg')}}" alt="...">
+                  <div class="caption">
+                    <h4 class="text-center">hot asphalt mixing plant
+                                            LB3000 (240TPH) Asphalt Mixing Plant</h4>
+                  </div>
+                </a>
+              </div>
+              <div class="col-xs-6 col-md-3">
+                <a href="#" class="thumbnail">
+                  <img src="{{asset('img/index/1.jpg')}}" alt="...">
+                  <div class="caption">
+                    <h4 class="text-center">hot asphalt mixing plant
+                                            LB3000 (240TPH) Asphalt Mixing Plant</h4>
+                  </div>
+                </a>
+              </div>
+            </div>
+        </div>
+
+        <br>
+        <div class="container">
+        	<div class="row">
+        		<div class="col-md-8" id="indextab">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <h3 class="panel-title"> >> </h3>
+                            <span class="pull-right">
+                                <!-- Tabs -->
+                                <ul class="nav panel-tabs">
+                                    <li class="active"><a href="#tab1" data-toggle="tab">ABOUT US</a></li>
+                                    <li><a href="#tab2" data-toggle="tab">SERVICE</a></li>
+                                </ul>
+                            </span>
+                        </div>
+                        <div class="panel-body">
+                            <div class="tab-content">
+                                <div class="tab-pane active" id="tab1">
+                                    <h3 class="text-center">Zhengzhou HANVY Machinery And <br/> Equipment Co., Ltd.</h3>
+                                    <p>
+                                    is the exportation department of Hanvy group of companies. Hanvy group of company is set up by 6 factories. The 6 factories are brothers companies located within Henan province, separately about 50-350km away from Zhengzhou the capital City of Henan province. In order to deal with commodity inspection, customs declaration, fast orders processing and urgent responds to the customer enquiries, quick responds to the customers after sales services, maintaining and satisfaction of the customers is our priority goal...
+                                    </p>
+                                    </div>
+                                <div class="tab-pane" id="tab2">
+                                    <h3 class="text-center">Hanvy provides installation, <br/>debugging service and easy wear parts</h3>
+                                    <p>We offer modeling test and test report for large final equipments, the customer is invited to our factory for visiting and observing the test.
+                                       Hanvy offers on-site field service for installation and debugging. Hanvy is committed to having all easy wear parts on hand to keep your project running.
+                                       </p>
+                                </div>
+
+                                </div>
+                            </div>
+                        </div>
+                </div>
+                <div class="col-md-4">
+                <div id="tour">
+                    <h4>FACTORY TOUR</h4>
+                    </div>
+                    <div class="thumbnail">
+                      <img src="{{asset('img/tour.jpg')}}" alt="...">
+                      <div class="caption">
+                        <p>Changchun Road11, Zhengzhou City, Henan Province,China</p>
+                        <p><a href="#" class="btn btn-primary btn-lg" role="button">Contact Us</a></p>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        	</div>
+
+        	<hr class="blueline" />
+
+        	<div class="container">
+        	    <ul class="list-inline text-center">
+                      <li><a href="">Asphalt Mixing Plant</a></li>
+                      <li>|</li>
+                      <li><a href="">Concrete Mixing Plant</a></li>
+                      <li>|</li>
+                      <li><a href="">Products</a></li>
+                      <li>|</li>
+                      <li><a href="">Index</a></li>
+                </ul>
+                <p class="text-center">Copyright © Zhengzhou Hanvy Machinery And Equipment Co., Ltd.</p>
+        	</div>
+
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
+        <script src="http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
         <script src="js/plugins.js"></script>
         <script src="js/main.js"></script>
     </body>
